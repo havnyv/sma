71c7cbab43e9597f9f3a04e4168323ef9c199913
Harshavardhan <havnyv@gmail.com>
2023-01-01 10:52:02 +0530

    basic plotting v0 done

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 import plotly
 import plotly.express as px
 import yfinance as yf
+import json
 
 app = Flask(__name__)
 
@@ -19,35 +20,38 @@ def index():
 def stock():
     return render_template('stock.html')
 
+
 @app.route('/callback/<endpoint>')
-def cb(endpoint):   
+def cb(endpoint):
     if endpoint == "getStock":
-        return gm(request.args.get('data'),request.args.get('period'),request.args.get('interval'))
+        return gm(request.args.get('data'))
     elif endpoint == "getInfo":
         stock = request.args.get('data')
-        st = yf.Ticker(stock)
-        return json.dumps(st.info)
+        df1 = pd.read_csv(f"./static/nifty_500_data/AWL.csv")
+        max = df1['Close'].max()
+        min = df1['Close'].min()
+        info = {"a52high": max, "a52low": min,
+                "shortName": "ntggg", "symbol": "hell"}
+        return json.dumps(info)
     else:
         return "Bad endpoint", 400
 
 # Return the JSON data for the Plotly graph
-def gm(stock,period, interval):
-    st = yf.Ticker(stock)
-  
+
+
+def gm(stock):
     # Create a line graph
-    df = st.history(period=(period), interval=interval)
-    df=df.reset_index()
-    df.columns = ['Date-Time']+list(df.columns[1:])
+    df = pd.read_csv("./static/ACC.csv")
+    df = df.reset_index()
     max = (df['Open'].max())
     min = (df['Open'].min())
     range = max - min
     margin = range * 0.05
     max = max + margin
     min = min - margin
-    fig = px.area(df, x='Date-Time', y="Open",
-        hover_data=("Open","Close","Volume"), 
-        range_y=(min,max), template="seaborn" )
-
+    fig = px.area(df, x='Date', y="Open",
+                  hover_data=("Open", "Close", "Volume"),
+                  range_y=(min, max), template="seaborn")
 
     # Create a JSON representation of the graph
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
